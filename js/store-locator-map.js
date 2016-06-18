4f77b4d12897e7d9fde698a88b92af09af5be48e
James Wilson <jrwilson3@gmail.com>
2016-06-18 08:25:46 -0500

    SO-81: Pass JS Lint.

--- js/store-locator-map.js
+++ js/store-locator-map.js
@@ -42,12 +42,12 @@
   }
 
   function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
+    if (!url) { url = window.location.href; }
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results) return '';
-    if (!results[2]) return '';
+    if (!results) { return ''; }
+    if (!results[2]) { return ''; }
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
