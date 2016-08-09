e035c8202b0b8c9330187dcb39adc4e05ee0224d
lewisnyman <hello@lewisnyman.co.uk>
2016-08-09 15:52:30 +0100

    SO-236: Fully functional mobile nav, visual details are WIP

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -7,28 +7,43 @@
 
   Drupal.behaviors.mobileNav = {
     isOpen: false,
+    isAnimating: false,
     attach: function (context) {
       var nav = this;
       nav.$context = $(context);
       nav.$wrapper = nav.$context.find('.mobile-nav');
       nav.$trigger = nav.$context.find('.mobile-nav__trigger');
+      nav.$body = $('body');
 
       nav.$trigger.on('click', function(e) {
         e.preventDefault();
+        if (nav.isAnimating) {
+          return false;
+        }
         nav.toggle();
       });
+
+      nav.$wrapper.on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          function(e) {
+        nav.isAnimating = false;
+      });
+
     },
     toggle: function () {
       var nav = this;
+      nav.isAnimating = true;
       if (nav.isOpen) {
         nav.$wrapper.removeClass('is-open');
+        nav.$body.removeClass('has-mobile-nav');
         nav.isOpen = false;
       }
       else {
         nav.$wrapper.addClass('is-open');
+        nav.$body.addClass('has-mobile-nav');
         nav.isOpen = true;
       }
-    },
+    }
   };
+
 
 }(jQuery, Drupal));
