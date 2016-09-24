6842428a619473a1e54ff6c5c947aaf195cdf767
lewisnyman <hello@lewisnyman.co.uk>
2016-09-24 18:45:22 +0100

    SO-473: Added support for no link in mobile nav

--- js/menu--expandable.js
+++ js/menu--expandable.js
@@ -9,16 +9,16 @@ $(document).ready(function($) {
     $this = $(this);
 
     // Make all top-level menu items expandable.
-    $('.menu__toplevel > .menu__item.menu__item--has-children', $this).attr('aria-expanded', 'false')
-      .find(' > a')
-        .after('<button class="menu__toggle"><span class="menu__toggle-horizontal"></span><span class="menu__toggle-vertical"></span></button>');
+    console.log($('.menu__toplevel > .menu__item.menu__item--has-children', $this).attr('aria-expanded', 'false')
+      .find(' > a, > .no-link')
+        .after('<button class="menu__toggle"><span class="menu__toggle-horizontal"></span><span class="menu__toggle-vertical"></span></button>'));
 
     // Ensure active menu items deep in a menu tree get expanded by default.
     $('.is-active', $this)
       .parents('.menu__item').attr('aria-expanded', 'true');
 
     // Toggle expand/collapsed state with button trigger and ARIA attribute.
-    $('.menu__toggle', $this).click(function() {
+    $('.menu__toggle, .no-link', $this).click(function() {
       if ($(this).closest('.menu__item').attr('aria-expanded') === 'true') {
         $(this).closest('.menu__item').attr('aria-expanded', 'false');
       } else {
